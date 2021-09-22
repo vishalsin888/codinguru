5e8ae724c572d873efe8a4061ef4559959383831
vishalsin888 <vishalsin888@gmail.com>
2021-09-23 00:37:40 +0530

    updated version2

--- js/index.js
+++ js/index.js
@@ -3,8 +3,17 @@ $(document).ready(function(){
         // alert();
        
         var target = $(this).attr("href");
-        console.log(target);
+       
+        $("html,body").animate({
+            scrollTop : $(target).offset().top-75,
+        },1000)
+        e.preventDefault();
+    });
 
+    $(".home_pg").on("click", function(e){
+        // alert();
+       
+        var target = $(this).attr("href");
         $("html,body").animate({
             scrollTop : $(target).offset().top-75,
         },1000)
